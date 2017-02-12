b68f2f795240c8e0d00b61ad853a345daadfd80c
_meugen_ <eugen35@mail.ru>
2017-02-12 15:51:19 +0500

    1. redux-persist работает теперь вроде бы вместе с redux-thunk 2. Удалил случайно попавшую в проект папку "src"

--- app/containers/app.js
+++ app/containers/app.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { persistStore, autoRehydrate } from 'redux-persist'
@@ -9,13 +9,14 @@ import { AsyncStorage } from 'react-native';
 import * as reducers from '../reducers';
 import CounterApp from './counterApp';
 
-//const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const reducer = combineReducers(reducers);
-// Add the autoRehydrate middleware to your redux store
-//const store = createStoreWithMiddleware(reducer,undefined,autoRehydrate());
-const store = createStore(reducer, undefined, autoRehydrate());
 
-// Enable persistence
+
+//const store = createStore(reducer, undefined, compose(applyMiddleware(thunk), autoRehydrate()));
+//вроде правильно, как ниже, а не как выше: см. раздел tips http://redux.js.org/docs/api/applyMiddleware.html
+const store = createStore(reducer, undefined, autoRehydrate(), applyMiddleware(thunk)); // Add the autoRehydrate middleware to your redux store
+
+// Enable persistence and choice AsyncStorage
 persistStore(store, {storage: AsyncStorage})
 
 export default class App extends Component {
