18168c3c13632a480a08d27cba72f841b41113dd
_meugen_ <eugen35@mail.ru>
2017-02-12 15:33:53 +0500

    redux-persist заработал, но нужно вернуть redux-thunk

--- app/containers/app.js
+++ app/containers/app.js
@@ -3,18 +3,20 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { persistStore, autoRehydrate } from 'redux-persist'
+import { AsyncStorage } from 'react-native';
 
 
 import * as reducers from '../reducers';
 import CounterApp from './counterApp';
 
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+//const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const reducer = combineReducers(reducers);
 // Add the autoRehydrate middleware to your redux store
-const store = createStoreWithMiddleware(reducer,undefined,autoRehydrate());
+//const store = createStoreWithMiddleware(reducer,undefined,autoRehydrate());
+const store = createStore(reducer, undefined, autoRehydrate());
 
 // Enable persistence
-persistStore(store)
+persistStore(store, {storage: AsyncStorage})
 
 export default class App extends Component {
   render() {
