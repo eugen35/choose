676a5f3df412fec72cc76c8be93d82472a56267e
_meugen_ <eugen35@mail.ru>
2017-02-12 09:32:25 +0500

    redux-persist try.... may be middleware string need change изза thunk

--- app/containers/app.js
+++ app/containers/app.js
@@ -2,13 +2,19 @@ import React, {Component} from 'react';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import { persistStore, autoRehydrate } from 'redux-persist'
+
 
 import * as reducers from '../reducers';
 import CounterApp from './counterApp';
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const reducer = combineReducers(reducers);
-const store = createStoreWithMiddleware(reducer);
+// Add the autoRehydrate middleware to your redux store
+const store = createStoreWithMiddleware(reducer,undefined,autoRehydrate());
+
+// Enable persistence
+persistStore(store)
 
 export default class App extends Component {
   render() {
